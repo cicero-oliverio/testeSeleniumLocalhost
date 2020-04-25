588045f642f506833cdabd378f0334b543f9fdd7
Cicero Oliverio <cicero_oliverio@hotmail.com>
2020-04-25 15:18:55 -0300

    Valida sistema operacional

--- src/main/java/utils/CreateChrome.java
+++ src/main/java/utils/CreateChrome.java
@@ -1,20 +1,29 @@
 package utils;
 
 import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CreateChrome {
 
-  public WebDriver createChrome(){
-    System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe");
-    WebDriver navigator = new ChromeDriver();
-    navigator.manage().window().maximize();
-    navigator.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    public WebDriver createChrome() {
 
-    return navigator;
+        String sistema = System.getProperty("os.name");
 
-  }
+        if (sistema.equals("Linux")) {
+            System.setProperty("webdriver.chrome.driver", "lib/chromedriver");
+        } else {
+            System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe");
+        }
+
+        WebDriver navigator = new ChromeDriver();
+        navigator.manage().window().maximize();
+        navigator.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        return navigator;
+
+    }
 
 }
 
