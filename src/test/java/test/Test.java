8fd64f8745738e9dbc6d30a97310f7d950725775
cicero-oliverio <cicero_oliverio@hotmail.com>
2020-12-26 17:18:45 -0300

    Melhorias diversas

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -11,24 +11,44 @@ public class Test {
   CreateChrome createChrome = new CreateChrome();
   private WebDriver navigator = createChrome.createChrome();
 
-  public  String nome = "Jair Henrique Melo";
-  public String numerocpf = "04863841876";
-  public String rg = "176744009";
+//  public  String nome = "Jair Henrique Melo";
+//  public String numerocpf = "04863841876";
+//  public String rg = "176744009";
+//  public String baseUrl = "http://www2.policiamilitar.sp.gov.br/ocorrenciaweb";
+//  public String logradouroCasa = "Estrada Brasílio de Lima";
+//  public String numero = "225";
+//  public String numerocep = "09822020";
+//  public String contato = "radioContatoNao";
+////  public String contato = "radioContatoSim";
+//  public String acompanhar = "radioAcompanharSim";
+////  public String acompanhar = "radioAcompanharNao";
+
+  public String nome = "Cicero Oliverio";
+  public String numerocpf = "33874389820";
+  public String rg = "420308696";
   public String baseUrl = "http://www2.policiamilitar.sp.gov.br/ocorrenciaweb";
-  public String logradouroCasa = "Estrada Brasílio de Lima";
-  public String numero = "4615";
-  public String numerocep = "09822020";
+  public String logradouroCasa = "Rua Felipe Camarao";
+  public String numero = "186";
+  public String numerocep = "09820140";
   public String contato = "radioContatoNao";
-//  public String contato = "radioContatoSim";
-  public String acompanhar = "radioAcompanharSim";
-//  public String acompanhar = "radioAcompanharNao";
+  //  public String contato = "radioContatoSim";
+// public String acompanhar = "radioAcompanharSim";
+  public String acompanhar = "radioAcompanharNao";
 
-//  public String reclamacao = "Todos os dias esse senhor passa o dia batendo.\n"
-//      + "Fica disfarçado no fundo da casa, ele começa logo cedo com bate-bate, furadeira, serra, sem a menor preocupação.Impossível trabalhar e estudar assim!\n"
-//      + "Pelo amor de Deus chamem ele na casa e nos ajudem!"; //Descrever a reclamação aqui
+//  public String reclamacao =
+//      "Desde cedo com uma máquina ligada serrando alguma coisa, já são 21:30 e nada de parar."
+//          + "Barulho muito alto, não tem acordo o vizinho disse que não vai parar, trabalho amanha e preciso dormir. Pelo amor de Deus nos ajudem!";
 
-  public String reclamacao = "Pancadão rolando solto em chalé dos dois lados da rua com muita bebida, drogas e barulho em plena quarentena."
-      + "O local é um ponto de venda de droga, maconha e cocaína, estão agressivos e ameaçando os vizinhos. Nos ajudem pelo amor de Deus!";
+  /* public String reclamacao = "Festa de aniversário a essa hora com a maior barulheira, gritaria, bebidas drogas impossível dormir com essa zona.Vizinhos alterados e agressivos, nos ajudem pelo amor de Deus!";
+   */
+
+//  public String reclamacao = "Em plena sexta-feira a essa hora com som alto, vizinho encrenqueiro e agressivo.\n"
+//      + "Estou com criança pequena em casa e tentando trabalhar mas não dá.\n"
+//      + "Ele ameaça os vizinhos e disse que a polícia nada pode fazer, pelo amor de Deus nos ajudem!";
+
+  public String reclamacao = "Em plena pandemia com classificação vermelha e acabaram de começar uma festinha em casa.\n"
+      + "Pessoas com diversas crianças, muita barulheira, muitos gritos e som bem alto.\n"
+      + "Todos estão sem máscara e completamente bêbados. Pelo amor de Deus que nos ajude!";
 
   @org.junit.Test
   public void residenciasFestas() throws InterruptedException {
@@ -89,7 +109,7 @@ public class Test {
 
     navigator.findElement(By.id(contato)).click();
 
-    if (contato.equals("radioContatoSim")){
+    if (contato.equals("radioContatoSim")) {
 
       navigator.findElement(By.id(acompanhar)).click();
 
@@ -145,7 +165,8 @@ public class Test {
 
     navigator.findElement(By.xpath("//*[@id=\"Pergunta_1\"]/div[3]/label")).click();
 
-    navigator.findElement(By.xpath("/html/body/div[1]/form/div[3]/div/div[3]/div/div[6]/div[6]/label/input")).click();
+    navigator.findElement(
+        By.xpath("/html/body/div[1]/form/div[3]/div/div[3]/div/div[6]/div[6]/label/input")).click();
 
     navigator.findElement(By.id("botaoOrigemProximo")).click();
 
@@ -159,7 +180,7 @@ public class Test {
 
     navigator.findElement(By.id(contato)).click();
 
-    if (contato.equals("radioContatoSim")){
+    if (contato.equals("radioContatoSim")) {
 
       navigator.findElement(By.id(acompanhar)).click();
 
@@ -215,7 +236,8 @@ public class Test {
 
     navigator.findElement(By.xpath("//*[@id=\"Pergunta_1\"]/div[3]/label")).click();
 
-    navigator.findElement(By.xpath("/html/body/div[1]/form/div[3]/div/div[3]/div/div[6]/div[2]/label/input")).click();
+    navigator.findElement(
+        By.xpath("/html/body/div[1]/form/div[3]/div/div[3]/div/div[6]/div[2]/label/input")).click();
 
     navigator.findElement(By.id("botaoOrigemProximo")).click();
 
@@ -229,7 +251,7 @@ public class Test {
 
     navigator.findElement(By.id(contato)).click();
 
-    if (contato.equals("radioContatoSim")){
+    if (contato.equals("radioContatoSim")) {
 
       navigator.findElement(By.id(acompanhar)).click();
 
@@ -285,7 +307,8 @@ public class Test {
 
     navigator.findElement(By.xpath("//*[@id=\"Pergunta_1\"]/div[3]/label")).click();
 
-    navigator.findElement(By.xpath("/html/body/div[1]/form/div[3]/div/div[3]/div/div[6]/div[3]/label/input")).click();
+    navigator.findElement(
+        By.xpath("/html/body/div[1]/form/div[3]/div/div[3]/div/div[6]/div[3]/label/input")).click();
 
     navigator.findElement(By.id("botaoOrigemProximo")).click();
 
@@ -299,7 +322,7 @@ public class Test {
 
     navigator.findElement(By.id(contato)).click();
 
-    if (contato.equals("radioContatoSim")){
+    if (contato.equals("radioContatoSim")) {
 
       navigator.findElement(By.id(acompanhar)).click();
 
@@ -371,7 +394,7 @@ public class Test {
 
     navigator.findElement(By.id(contato)).click();
 
-    if (contato.equals("radioContatoSim")){
+    if (contato.equals("radioContatoSim")) {
 
       navigator.findElement(By.id(acompanhar)).click();
 
@@ -443,7 +466,7 @@ public class Test {
 
     navigator.findElement(By.id(contato)).click();
 
-    if (contato.equals("radioContatoSim")){
+    if (contato.equals("radioContatoSim")) {
 
       navigator.findElement(By.id(acompanhar)).click();
 
@@ -501,7 +524,8 @@ public class Test {
 
     navigator.findElement(By.xpath("//*[@id=\"Pergunta_2\"]/div[7]/label")).click();
 
-    navigator.findElement(By.xpath("/html/body/div[1]/form/div[3]/div/div[3]/div/div[2]/div[3]/label/input")).click();
+    navigator.findElement(
+        By.xpath("/html/body/div[1]/form/div[3]/div/div[3]/div/div[2]/div[3]/label/input")).click();
 
     navigator.findElement(By.id("botaoOrigemProximo")).click();
 
@@ -515,7 +539,7 @@ public class Test {
 
     navigator.findElement(By.id(contato)).click();
 
-    if (contato.equals("radioContatoSim")){
+    if (contato.equals("radioContatoSim")) {
 
       navigator.findElement(By.id(acompanhar)).click();
 
@@ -548,7 +572,7 @@ public class Test {
 
     //navigator.findElement(By.id("municipio")).click();
     WebElement municipio = navigator
-            .findElement(By.xpath("//*[contains(text(), 'SAO BERNARDO DO CAMPO')]"));
+        .findElement(By.xpath("//*[contains(text(), 'SAO BERNARDO DO CAMPO')]"));
     municipio.click();
 
     WebElement logradouro = navigator.findElement(By.id("logradouro"));
@@ -573,9 +597,11 @@ public class Test {
 
     navigator.findElement(By.xpath("//*[@id=\"Pergunta_2\"]/div[7]/label")).click();
 
-    navigator.findElement(By.xpath("/html/body/div[1]/form/div[3]/div/div[3]/div/div[2]/div[5]/label/input")).click();
+    navigator.findElement(
+        By.xpath("/html/body/div[1]/form/div[3]/div/div[3]/div/div[2]/div[5]/label/input")).click();
 
-    navigator.findElement(By.xpath("/html/body/div[1]/form/div[3]/div/div[3]/div/div[3]/div[3]/label/input")).click();
+    navigator.findElement(
+        By.xpath("/html/body/div[1]/form/div[3]/div/div[3]/div/div[3]/div[3]/label/input")).click();
 
     navigator.findElement(By.id("botaoOrigemProximo")).click();
 
@@ -589,7 +615,83 @@ public class Test {
 
     navigator.findElement(By.id(contato)).click();
 
-    if (contato.equals("radioContatoSim")){
+    if (contato.equals("radioContatoSim")) {
+
+      navigator.findElement(By.id(acompanhar)).click();
+
+    }
+
+    navigator.findElement(By.id("checkDeclaracao")).click();
+
+  }
+
+  @org.junit.Test
+  public void viaPublicaCriancas() throws InterruptedException {
+
+    navigator.get(baseUrl);
+
+    //Primeira Página
+
+    navigator.findElement(By.id("nome")).sendKeys(nome);
+    Thread.sleep(1000);
+    WebElement cpf = navigator.findElement(By.id("cpf"));
+    cpf.click();
+    cpf.sendKeys(numerocpf);
+    navigator.findElement(By.id("rg")).sendKeys(rg);
+    navigator.findElement(By.id("email")).sendKeys("cicero_oliverio@hotmail.com");
+    navigator.findElement(By.id("emailVerifique")).sendKeys("cicero_oliverio@hotmail.com");
+    navigator.findElement(By.id("ddd")).sendKeys("11");
+    navigator.findElement(By.id("telefone")).sendKeys("975697393");
+    navigator.findElement(By.id("botaoCadastroProximo")).click();
+
+    //Segunda Página
+
+    //navigator.findElement(By.id("municipio")).click();
+    WebElement municipio = navigator
+        .findElement(By.xpath("//*[contains(text(), 'SAO BERNARDO DO CAMPO')]"));
+    municipio.click();
+
+    WebElement logradouro = navigator.findElement(By.id("logradouro"));
+
+    logradouro.sendKeys(logradouroCasa);
+    logradouro.sendKeys(Keys.TAB);
+
+    navigator.findElement(By.xpath("//*[@id=\"tabelaLogradouro\"]/tbody/tr/td[2]")).click();
+
+    navigator.findElement(By.id("numero")).sendKeys(numero);
+
+    navigator.findElement(By.id("complemento")).sendKeys("Casa");
+    WebElement cep = navigator.findElement(By.id("cep"));
+    cep.click();
+    cep.sendKeys(numerocep);
+
+    navigator.findElement(By.id("botaoEnderecoProximo")).click();
+
+    // Terceira Página
+
+    navigator.findElement(By.xpath("//*[@id=\"Pergunta_1\"]/div[2]/label")).click();
+
+        navigator.findElement(By.xpath("//*[@id=\"Pergunta_2\"]/div[8]/label")).click();
+
+    navigator.findElement(
+        By.xpath("/html/body/div[1]/form/div[3]/div/div[3]/div/div[2]/div[8]/label/input")).click();
+
+    navigator.findElement(
+        By.xpath("/html/body/div[1]/form/div[3]/div/div[3]/div/div[3]/div[3]/label/input")).click();
+
+    navigator.findElement(By.id("botaoOrigemProximo")).click();
+
+    // Última Página
+
+    WebElement caixadeTexto = navigator.findElement(By.id("textoComplemento"));
+
+    caixadeTexto.click();
+
+    caixadeTexto.sendKeys(reclamacao);
+
+    navigator.findElement(By.id(contato)).click();
+
+    if (contato.equals("radioContatoSim")) {
 
       navigator.findElement(By.id(acompanhar)).click();
 
